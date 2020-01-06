990655cd6e80c9e2aa4e9678253a2e2f93a2da82
lsh5665256 <490830076@qq.com>
2020-01-06 18:49:51 +0800

    登录排版

---
+++ src/views/cart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Cart:React.FC=()=>{
+    return (
+        <div className="cart">
+            cart
+        </div>
+    )
+}
+
+export default Cart;
---
+++ src/views/homes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+import View from "../router/routerView"
+const Homes:React.FC=(props:any)=>{
+    return (
+        <div className="homes">
+          
+            <div className="main">
+            <View route={props.child}></View>
+            </div>
+            <div className="footer">
+                <p><NavLink to="/home/main">首页</NavLink></p>
+                <p><NavLink to="/home/subject">专题</NavLink></p>
+                <p><NavLink to="/home/classify">分类</NavLink></p>            
+                <p><NavLink to="/home/cart">购物车</NavLink></p>
+                <p><NavLink to="/home/my">我的</NavLink></p>
+            </div>
+        </div>
+    )
+}
+
+export default Homes;
+
